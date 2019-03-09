5ca26f47402fca6dd4820b4f520244d5d1195de7
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-08 19:29:51 -0500

    Added eat method in mammal

--- src/main/java/com/zipcodewilmington/froilansfarm/crops/Egg.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/crops/Egg.java
@@ -1,7 +1,5 @@
 package com.zipcodewilmington.froilansfarm.crops;
 
-import com.zipcodewilmington.froilansfarm.crops.Edible;
-
 public class Egg extends Edible {
 
     private Boolean hasBeenFertilized;
