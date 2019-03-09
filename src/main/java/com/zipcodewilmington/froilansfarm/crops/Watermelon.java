4bd825e1cdcbb46f6fea99e0e67c8669083e1427
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-08 20:29:40 -0500

    Storing change

--- src/main/java/com/zipcodewilmington/froilansfarm/crops/Watermelon.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/crops/Watermelon.java
@@ -3,11 +3,6 @@ package com.zipcodewilmington.froilansfarm.crops;
 public class Watermelon extends Edible {
 
     public Watermelon(){
-        this(1371);
+        super(1371);
     }
-
-    private Watermelon(Integer caloriecount){
-        super(caloriecount);
-    }
-
 }
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/crops/CropRowTest.java
@@ -0,0 +1,33 @@
+package com.zipcodewilmington.froilansfarm.crops;
+
+import org.junit.Test;
+
+public class CropRowTest {
+
+    @Test
+    public void nullaryConstructorTest(){
+
+
+    }
+
+    @Test
+    public void addCropsTest(){
+
+    }
+
+    @Test
+    public void removeCropsTest(){
+
+    }
+
+    @Test
+    public void getListTest(){
+
+    }
+
+    @Test
+    public void fertilizeTest(){
+
+    }
+
+}
